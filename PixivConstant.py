414388c679e78a725ee58f69bfaaf063b1fc92bb
Adi Irawan <nchek2000@gmail.com>
2012-04-21 09:01:34 +0800

    v20120421

--- PixivConstant.py
+++ PixivConstant.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-PIXIVUTIL_VERSION = '20120413'
+PIXIVUTIL_VERSION = '20120421'
 PIXIVUTIL_LINK = 'http://nandaka.wordpress.com/tag/pixiv-downloader/'
 PIXIV_URL = 'http://www.pixiv.net'
 PIXIV_CSS_LIST_ID = 'display_works'
