a7ea26d07c7e8cc700d95c388c06b60b4fbf993e
Adi Irawan <nchek2000@gmail.com>
2012-06-09 22:21:38 +0800

    v20120609

--- PixivConstant.py
+++ PixivConstant.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-PIXIVUTIL_VERSION = '20120601'
+PIXIVUTIL_VERSION = '20120609'
 PIXIVUTIL_LINK = 'https://nandaka.wordpress.com/tag/pixiv-downloader/'
 PIXIV_URL = 'http://www.pixiv.net'
 PIXIV_URL_SSL = 'https://ssl.pixiv.net/login.php'
