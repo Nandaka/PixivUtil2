40bbf3a2c13611ea4aae7342c29fbecc3de30fc2
Nandaka <nchek2000@gmail.com>
2013-05-10 23:19:59 +0800

    Fix error code definition

--- PixivException.py
+++ PixivException.py
@@ -1,27 +1,27 @@
 # -*- coding: UTF-8 -*-
 
-NOT_LOGGED_IN      = 100
-USER_ID_NOT_EXISTS = 1001
-USER_ID_SUSPENDED  = 1002
-OTHER_MEMBER_ERROR = 1003
-NO_IMAGES          = 1004
-
-PARSE_TOKEN_DIFFERENT_IMAGE_STRUCTURE = 1005
-PARSE_TOKEN_PARSE_NO_IMAGES           = 1006
-NO_PAGE_GIVEN                         = 1007
-
-FILE_NOT_EXISTS_OR_NO_WRITE_PERMISSION = 4002
-FILE_NOT_EXISTS_OR_NO_READ_PERMISSION  = 4001
-
-OTHER_IMAGE_ERROR    = 2001
-NOT_IN_MYPICK        = 2002
-NO_APPROPRIATE_LEVEL = 2003
-IMAGE_DELETED        = 2004
-R_18_DISABLED        = 2005
-UNKNOWN_IMAGE_ERROR  = 2006
-
-
 class PixivException(Exception):
+  ## Error Codes
+  NOT_LOGGED_IN      = 100
+  USER_ID_NOT_EXISTS = 1001
+  USER_ID_SUSPENDED  = 1002
+  OTHER_MEMBER_ERROR = 1003
+  NO_IMAGES          = 1004
+
+  PARSE_TOKEN_DIFFERENT_IMAGE_STRUCTURE = 1005
+  PARSE_TOKEN_PARSE_NO_IMAGES           = 1006
+  NO_PAGE_GIVEN                         = 1007
+
+  FILE_NOT_EXISTS_OR_NO_WRITE_PERMISSION = 4002
+  FILE_NOT_EXISTS_OR_NO_READ_PERMISSION  = 4001
+
+  OTHER_IMAGE_ERROR    = 2001
+  NOT_IN_MYPICK        = 2002
+  NO_APPROPRIATE_LEVEL = 2003
+  IMAGE_DELETED        = 2004
+  R_18_DISABLED        = 2005
+  UNKNOWN_IMAGE_ERROR  = 2006
+
   errorCode = 0
   
   def __init__(self, value, errorCode):
@@ -30,4 +30,4 @@ class PixivException(Exception):
     self.errorCode = errorCode
     
   def __str__(self):
-    return str(errorCode) + " " + repr(self.value)
+    return str(self.errorCode) + " " + repr(self.value)
