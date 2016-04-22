a60b94bf94e6e28dcd65299f63e8ebe2a0121450
Nandaka <nchek2000@gmail.com>
2016-04-22 20:59:52 +0800

    update test page

--- test.PixivModel.py
+++ test.PixivModel.py
@@ -34,7 +34,7 @@ class TestPixivArtist(unittest.TestCase):
       self.assertNotEqual(artist, None)
       self.assertEqual(artist.artistId, 1107124)
       self.assertEqual(artist.artistToken, 'kirabara29')
-      self.assertEqual(artist.totalImages, 69)
+      self.assertEqual(artist.totalImages, 71)
 
     def testPixivArtistNoImage(self):
       #print '\nTesting member page - no image'
@@ -491,7 +491,7 @@ class TestPixivTags(unittest.TestCase):
 
         self.assertEqual(len(image.itemList), 20)
         self.assertEqual(image.isLastPage, False)
-        self.assertEqual(image.availableImages, 2288)
+        self.assertEqual(image.availableImages, 2289)
 
     ## tags.php?tag=%E3%81%93%E3%81%AE%E4%B8%AD%E3%81%AB1%E4%BA%BA%E3%80%81%E5%A6%B9%E3%81%8C%E3%81%84%E3%82%8B%21
     def testTagsSearchExact(self):
