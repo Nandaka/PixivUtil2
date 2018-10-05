fefd48ab1a296fe3e2ef0bd3e0bd8b881e6b806a
Nandaka <nchek2000@gmail.com>
2018-10-06 00:26:33 +0800

    update vers

--- PixivConstant.py
+++ PixivConstant.py
@@ -2,7 +2,7 @@
 # pylint: disable=I0011, C, C0302
 
 
-PIXIVUTIL_VERSION = '20181005-alpha1'
+PIXIVUTIL_VERSION = '20181006-alpha2'
 PIXIVUTIL_LINK = 'https://github.com/Nandaka/PixivUtil2/releases'
 PIXIVUTIL_DONATE = 'https://bit.ly/PixivUtilDonation'
 PIXIV_URL = 'https://www.pixiv.net'
