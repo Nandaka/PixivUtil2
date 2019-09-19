10372782ea2623348fcba377c322c4d03965ad83
Nandaka <nchek2000@gmail.com>
2019-09-19 13:42:56 +0800

    fix #530

--- PixivConstant.py
+++ PixivConstant.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-PIXIVUTIL_VERSION = '20190907b'
+PIXIVUTIL_VERSION = '20190919'
 PIXIVUTIL_LINK = 'https://github.com/Nandaka/PixivUtil2/releases'
 PIXIVUTIL_DONATE = 'https://bit.ly/PixivUtilDonation'
 PIXIV_URL = 'https://www.pixiv.net'
--- PixivOAuth.py
+++ PixivOAuth.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from datetime import datetime
+import md5
 
 import PixivHelper
 from PixivHelper import LocalUTCOffsetTimezone
@@ -55,13 +56,17 @@ class PixivOAuth(object):
         return values
 
     def _get_default_headers(self):
+        # fix #530
+        time = "{0}{1}".format(datetime.now().isoformat()[:19], self._tzInfo)
+        secret = "28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c"
+        time_hash = md5.md5("{0}{1}".format(time, secret))
         return {'User-Agent': 'PixivAndroidApp/5.0.136 (Android 6.0; Google Pixel C - 6.0.0 - API 23 - 2560x1800)',
                        'Accept-Language': 'en_US',
                        'App-OS': 'android',
                        'App-OS-Version': '4.4.2',
                        'App-Version': '5.0.136',
-                       'X-Client-Time': "{0}{1}".format(datetime.now().isoformat()[:19], self._tzInfo),
-                       'X-Client-Hash': '8827231063d3b75330d956b6a65e5955'}
+                       'X-Client-Time': time,
+                       'X-Client-Hash': time_hash.hexdigest()}
 
     def _get_headers_with_bearer(self):
         if self._access_token is None:
