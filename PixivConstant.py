f63029c255b929e5b2e3b7905011d2f9477c7f02
Nandaka <nchek2000@gmail.com>
2019-12-18 22:33:49 +0800

    remove old constant

--- PixivConstant.py
+++ PixivConstant.py
@@ -3,19 +3,6 @@
 PIXIVUTIL_VERSION = '20191218'
 PIXIVUTIL_LINK = 'https://github.com/Nandaka/PixivUtil2/releases'
 PIXIVUTIL_DONATE = 'https://bit.ly/PixivUtilDonation'
-PIXIV_URL = 'https://www.pixiv.net'
-PIXIV_URL_SSL = 'https://www.pixiv.net/login.php'
-PIXIV_CSS_LIST_ID = 'display_works'
-PIXIV_CSS_PROFILE_NAME_CLASS = 'f18b'
-PIXIV_CSS_IMAGE_TITLE_CLASS = 'works_data'
-PIXIV_CSS_TAGS_ID = 'tags'
-PIXIVUTIL_MODE_UPDATE_ONLY = '1'
-
-# Login Settings
-PIXIVUTIL_MODE_OVERWRITE = '2'
-PIXIV_LOGIN_URL = '/login.php'
-PIXIV_FORM_NUMBER = 1
-PIXIV_FORM_NUMBER_SSL = 1
 
 # Log Settings
 PIXIVUTIL_LOG_FILE = 'pixivutil.log'
