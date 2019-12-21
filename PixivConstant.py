6f8116a8ebf4776c8270122ae083d0be8bdb7488
Nandaka <nchek2000@gmail.com>
2019-12-21 10:27:49 +0800

    update version

--- PixivConstant.py
+++ PixivConstant.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-PIXIVUTIL_VERSION = '20191220-beta1'
+PIXIVUTIL_VERSION = '20191221'
 PIXIVUTIL_LINK = 'https://github.com/Nandaka/PixivUtil2/releases'
 PIXIVUTIL_DONATE = 'https://bit.ly/PixivUtilDonation'
 
