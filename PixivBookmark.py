28ef82914a5c8e8fca9f8bff1280b50c1110e6c3
Nandaka <nchek2000@gmail.com>
2020-01-12 21:59:09 +0800

    Update #623

--- PixivBookmark.py
+++ PixivBookmark.py
@@ -7,27 +7,43 @@ import json
 import re
 from datetime import datetime
 
+from bs4 import BeautifulSoup
+
 
 class PixivBookmark(object):
     '''Class for parsing Bookmarks'''
     __re_imageULItemsClass = re.compile(r".*\b_image-items\b.*")
 
     @staticmethod
-    def parseBookmark(page, root_directory, db_path):
+    def parseBookmark(page, root_directory, db_path, locale='en', is_json=False):
         '''Parse favorite artist page'''
         from PixivDBManager import PixivDBManager
         bookmarks = list()
+        result2 = list()
         db = PixivDBManager(root_directory=root_directory, target=db_path)
-        __re_member = re.compile(r'en/users/(\d*)')
-        result = page.find(attrs={'class': 'members'}).findAll('a')
 
-        # filter duplicated member_id
-        d = collections.OrderedDict()
-        for r in result:
-            member_id = __re_member.findall(r['href'])
-            if len(member_id) > 0:
-                d[member_id[0]] = member_id[0]
-        result2 = list(d.keys())
+        if is_json:
+            parsed = json.loads(page)
+            for member in parsed["body"]["users"]:
+                result2.append(member["userId"])
+        else:
+            # old method
+            parse_page = BeautifulSoup(page, features="html5lib")
+            __re_member = re.compile(locale + r'/users/(\d*)')
+
+            member_list = parse_page.find(attrs={'class': 'members'})
+            result = member_list.findAll('a')
+
+            # filter duplicated member_id
+            d = collections.OrderedDict()
+            for r in result:
+                member_id = __re_member.findall(r['href'])
+                if len(member_id) > 0:
+                    d[member_id[0]] = member_id[0]
+            result2 = list(d.keys())
+
+            parse_page.decompose()
+            del parse_page
 
         for r in result2:
             item = db.selectMemberByMemberId2(r)
--- PixivUtil2.py
+++ PixivUtil2.py
@@ -1185,31 +1185,43 @@ def get_bookmarks(hide, start_page=1, end_page=0, member_id=None):
     """Get User's bookmarked artists """
     total_list = list()
     i = start_page
+    limit = 24
+    offset = 0
+    is_json = False
+
     while True:
         if end_page != 0 and i > end_page:
             print('Limit reached')
             break
-        PixivHelper.print_and_log('info', 'Exporting page ' + str(i))
-        url = 'https://www.pixiv.net/bookmark.php?type=user&p=' + str(i)
+        PixivHelper.print_and_log('info', f'Exporting page {i}')
+        if member_id:
+            is_json = True
+            offset = 24 * (i - 1)
+            url = f'https://www.pixiv.net/ajax/user/{member_id}/following?offset={offset}&limit={limit}'
+        else:
+            url = f'https://www.pixiv.net/bookmark.php?type=user&p={i}'
         if hide:
             url = url + "&rest=hide"
-        if member_id:
-            url = url + "&id=" + member_id
-        PixivHelper.print_and_log('info', "Source URL: " + url)
+        else:
+            url = url + "&rest=show"
+
+        PixivHelper.print_and_log('info', f"Source URL: {url}")
 
         page = __br__.open_with_retry(url)
+        page_str = page.read().decode('utf8')
+        page.close()
 
-        parse_page = BeautifulSoup(page.read().decode("utf-8"), features="html5lib")
-        bookmarks = PixivBookmark.parseBookmark(parse_page, root_directory=__config__.rootDirectory, db_path=__config__.dbPath)
+        bookmarks = PixivBookmark.parseBookmark(page_str,
+                                                root_directory=__config__.rootDirectory,
+                                                db_path=__config__.dbPath,
+                                                is_json=is_json)
+
         if len(bookmarks) == 0:
             print('No more data')
             break
         total_list.extend(bookmarks)
         i = i + 1
         print(str(len(bookmarks)), 'items')
-        page.close()
-        parse_page.decompose()
-        del parse_page
         wait()
     return total_list
 
