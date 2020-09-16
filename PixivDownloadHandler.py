cbca1e7d48a1455d4ba69f74651f13ba86e76e8d
Nandaka <nchek2000@gmail.com>
2020-09-16 15:06:45 +0800

    Fix #796 add try-catch if fail to convert aPNG

--- PixivDownloadHandler.py
+++ PixivDownloadHandler.py
@@ -325,7 +325,13 @@ def handle_ugoira(image, filename, config, notifier):
     if config.createApng:
         gif_filename = ugo_name[:-7] + ".png"
         if not os.path.exists(gif_filename):
-            PixivHelper.ugoira2apng(ugo_name, gif_filename, config.deleteUgoira, image=image)
+            try:
+                PixivHelper.ugoira2apng(ugo_name, gif_filename, config.deleteUgoira, image=image)
+            except Exception:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                traceback.print_exception(exc_type, exc_value, exc_traceback)
+                PixivHelper.print_and_log('error', f'Error at handle_ugoira(): {sys.exc_info()} at when converting image id: {image.imageId} to aPNG.')
+
     if config.createWebm:
         gif_filename = ugo_name[:-7] + "." + config.ffmpegExt
         if not os.path.exists(gif_filename):
