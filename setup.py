2a5145e6f4ff14c19a6763b3e1b6a2f39e03615a
Nandaka <nchek2000@gmail.com>
2020-12-07 23:21:18 +0800

    update setup.py to read unicode readme.md

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ install_requires = [x.strip() for x in install_requires]
 
 # get long_description
 readme_path = convert_path('readme.md')
-with open(readme_path) as readme_file:
+with open(readme_path, 'r', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setup(
