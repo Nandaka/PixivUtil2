4988ad47da69f8c3ec4d18e2e046e30a9ded5857
amatuerCoder <17560146+bluerthanever@users.noreply.github.com>
2021-01-29 20:11:49 +0800

    Update test_PixivModel_whitecube.py

--- test_PixivModel_whitecube.py
+++ test_PixivModel_whitecube.py
@@ -84,7 +84,7 @@ class TestPixivModel_WhiteCube(unittest.TestCase):
         member.ParseInfo(info, False, False)
 
         member.PrintInfo()
-        self.assertEqual(member.totalImages, 184)
+        self.assertEqual(member.totalImages, 183)
         self.assertTrue(member.isLastPage)
         self.assertEqual(member.artistId, 14095911)
         self.assertTrue(member.haveImages)
