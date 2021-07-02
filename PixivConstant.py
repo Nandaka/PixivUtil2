49f77438390f5f553813aad2ca59b512fc97a16b
Nandaka <nchek2000@gmail.com>
2021-07-02 09:59:49 +0800

    update version and changelog

--- PixivConstant.py
+++ PixivConstant.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-PIXIVUTIL_VERSION = '20210421'
+PIXIVUTIL_VERSION = '20210702'
 PIXIVUTIL_LINK = 'https://github.com/Nandaka/PixivUtil2/releases'
 PIXIVUTIL_DONATE = 'https://bit.ly/PixivUtilDonation'
 
