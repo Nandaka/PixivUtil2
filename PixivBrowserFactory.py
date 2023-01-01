7e9cfb6fb5c21dea3ed9038e4a9ab5f504253dfc
PatrickL546 <75874561+PatrickL546@users.noreply.github.com>
2022-12-31 23:51:05 -0500

    Add back 'temp fix? #1209' commit

--- PixivBrowserFactory.py
+++ PixivBrowserFactory.py
@@ -158,8 +158,8 @@ class PixivBrowser(mechanize.Browser):
         # self.visit_response
         self.addheaders = [('User-agent', config.useragent)]
 
-        # force utf-8, fix issue #184
-        self.addheaders += [('Accept-Charset', 'utf-8')]
+        # # force utf-8, fix issue #184
+        # self.addheaders += [('Accept-Charset', 'utf-8')]
 
         socket.setdefaulttimeout(config.timeout)
 
@@ -343,10 +343,10 @@ class PixivBrowser(mechanize.Browser):
             PixivHelper.print_and_log('info', 'Trying to log in with saved cookie')
             self.clearCookie()
             self._loadCookie(login_cookie, "pixiv.net")
-            res = self.open_with_retry('https://www.pixiv.net/')
+            res = self.open_with_retry('https://www.pixiv.net/en')  # + self._locale)
             parsed = BeautifulSoup(res, features="html5lib")
             parsed_str = str(parsed.decode('utf-8'))
-            PixivHelper.get_logger().info('Logging in, return url: %s', res.geturl())
+            PixivHelper.print_and_log("info", f'Logging in, return url: {res.geturl()}')
             res.close()
             parsed.decompose()
             del parsed
