e6802d37897ef9ba225d6449a72952ac6386c39a
rts236 <208051351+rts236@users.noreply.github.com>
2025-04-20 22:44:33 +0200

    Test test_PixivModel_whitecube.py update with new Pixiv format
    With the following changes, `test_PixivModel_whitecube.py` passes.
    * Errors from url are now checked at every instance where the url is parsed, so there's no need for error checking at `PixivArtist.py` nor `testParseMemberError()` test.
    * Files `userdetail-` are now actually the ajax JSON from the first image of the artist. This is because it's the only place I know of an `artistToken` can be taken from.
    * Due to this, `artistAvatar` requires a `ParseInfo()` call using a third JSON url. Thus, maybe another test should be added for this.

--- test_PixivModel_whitecube.py
+++ test_PixivModel_whitecube.py
@@ -34,50 +34,54 @@ class TestPixivModel_WhiteCube(unittest.TestCase):
 #        image.PrintInfo()
 #        self.assertEqual(image.imageMode, "manga")
 
-    def testParseMemberError(self):
-        p = open('./test/ajax-error.json', 'r')
-        try:
-            PixivArtist(14095911, p.read())
-            self.fail("Exception expected.")
-        except PixivException as ex:
-            self.assertTrue(ex.errorCode == PixivException.OTHER_MEMBER_ERROR)
+#    def testParseMemberError(self):
+#        p = open('./test/ajax-error.json', 'r')
+#        payload = json.loads(p.read())
+#        try:
+#            PixivArtist(14095911, payload["body"])
+#            self.fail("Exception expected.")
+#        except PixivException as ex:
+#            self.assertTrue(ex.errorCode == PixivException.OTHER_MEMBER_ERROR)
 
     def testParseMemberImages(self):
         p = open('./test/all-14095911.json', 'r')
-        member = PixivArtist(14095911, p.read(), False, 0, 24)
+        payload = json.loads(p.read())
+        member = PixivArtist(14095911, payload["body"], False, 0, 24)
         self.assertIsNotNone(member)
         p2 = open('./test/userdetail-14095911.json', 'r')
         info = json.loads(p2.read())
         member.ParseInfo(info, False, False)
 
         member.PrintInfo()
-        self.assertEqual(member.artistId, 14095911)
+        self.assertEqual(int(member.artistId), 14095911)
         self.assertTrue(member.haveImages)
         self.assertFalse(member.isLastPage)
         self.assertEqual(member.artistToken, 'p199451')
         self.assertGreater(member.totalImages, 70)
-        self.assertEqual(member.artistAvatar, "https://i.pximg.net/user-profile/img/2018/02/01/23/55/46/13768317_dc1474fef3dc982ade71c1b2dbf32d9e.png")
+#        self.assertEqual(member.artistAvatar, "https://i.pximg.net/user-profile/img/2018/02/01/23/55/46/13768317_dc1474fef3dc982ade71c1b2dbf32d9e.png")
 
     def testParseMemberImages2(self):
         p = open('./test/all-26357.json', 'r')
-        member = PixivArtist(26357, p.read(), False, 0, 24)
+        payload = json.loads(p.read())
+        member = PixivArtist(26357, payload["body"], False, 0, 24)
         self.assertIsNotNone(member)
         p2 = open('./test/userdetail-26357.json', 'r')
         info = json.loads(p2.read())
         member.ParseInfo(info, False, False)
 
         member.PrintInfo()
-        self.assertEqual(member.artistId, 26357)
+        self.assertEqual(int(member.artistId), 26357)
         self.assertTrue(member.haveImages)
         self.assertFalse(member.isLastPage)
         self.assertEqual(member.artistToken, 'yukimaruko')
         self.assertGreater(member.totalImages, 7)
-        self.assertEqual(member.artistAvatar, "https://s.pximg.net/common/images/no_profile.png")
+#        self.assertEqual(member.artistAvatar, "https://s.pximg.net/common/images/no_profile.png")
 
     # https://www.pixiv.net/ajax/user/14095911/profile/all
     def testParseMemberImagesLastPage(self):
         p = open('./test/all-14095911.json', 'r')
-        member = PixivArtist(14095911, p.read(), False, 144, 48)
+        payload = json.loads(p.read())
+        member = PixivArtist(14095911, payload["body"], False, 144, 48)
         self.assertIsNotNone(member)
         p2 = open('./test/userdetail-14095911.json', 'r')
         info = json.loads(p2.read())
@@ -86,27 +90,29 @@ class TestPixivModel_WhiteCube(unittest.TestCase):
         member.PrintInfo()
         self.assertEqual(member.totalImages, 183)
         self.assertTrue(member.isLastPage)
-        self.assertEqual(member.artistId, 14095911)
+        self.assertEqual(int(member.artistId), 14095911)
         self.assertTrue(member.haveImages)
 
     # /ajax/user/14095911/illustmanga/tag?tag=R-18&offset=0&limit=48
     def testParseMemberImagesByTags(self):
         p = open('./test/tag-R-18-14095911.json', 'r')
-        member = PixivArtist(14095911, p.read(), False, 0, 24)
+        payload = json.loads(p.read())
+        member = PixivArtist(14095911, payload["body"], False, 0, 24)
         self.assertIsNotNone(member)
         p2 = open('./test/userdetail-14095911.json', 'r')
         info = json.loads(p2.read())
         member.ParseInfo(info, False, False)
 
         member.PrintInfo()
-        self.assertEqual(member.artistId, 14095911)
+        self.assertEqual(int(member.artistId), 14095911)
         self.assertTrue(member.haveImages)
         self.assertFalse(member.isLastPage)
 
     # /ajax/user/14095911/illustmanga/tag?tag=R-18&offset=48&limit=48
     def testParseMemberImagesByTagsLastPage(self):
         p = open('./test/tag-R-18-14095911-lastpage.json', 'r')
-        member = PixivArtist(14095911, p.read(), False, self.testParseMemberImagesByTagsLastPageOffset, 48)
+        payload = json.loads(p.read())
+        member = PixivArtist(14095911, payload["body"], False, self.testParseMemberImagesByTagsLastPageOffset, 48)
         self.assertIsNotNone(member)
         p2 = open('./test/userdetail-14095911.json', 'r')
         info = json.loads(p2.read())
@@ -115,20 +121,21 @@ class TestPixivModel_WhiteCube(unittest.TestCase):
         member.PrintInfo()
         self.assertEqual(member.totalImages, 85)
         self.assertTrue(member.isLastPage)
-        self.assertEqual(member.artistId, 14095911)
+        self.assertEqual(int(member.artistId), 14095911)
         self.assertTrue(member.haveImages)
         print(f"Last Image = {member.imageList[-1]}")
 
     def testParseMemberBookmarksByTags(self):
         p = open('./test/bookmarks-1039353.json', 'r')
-        member = PixivArtist(1039353, p.read(), False, 0, 24)
+        payload = json.loads(p.read())
+        member = PixivArtist(1039353, payload["body"], False, 0, 24)
         self.assertIsNotNone(member)
         p2 = open('./test/userdetail-1039353.json', 'r')
         info = json.loads(p2.read())
         member.ParseInfo(info, False, True)
 
         member.PrintInfo()
-        self.assertEqual(member.artistId, 1039353)
+        self.assertEqual(int(member.artistId), 1039353)
         self.assertTrue(member.haveImages)
         self.assertFalse(member.isLastPage)
 
